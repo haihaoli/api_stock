e54f4f955d6433007293dc9c4bab3b0c5037e42b
lihaihao <mashy1024@gmail.com>
2017-09-13 17:17:05 +0800

    edit show page

--- app/controllers/usstocks_controller.rb
+++ app/controllers/usstocks_controller.rb
@@ -4,28 +4,18 @@ class UsstocksController < ApplicationController
     @usstocks = Usstock.all
   end
 
-  def new
-    @usstock = Usstock.new
-  end
-
-  def create
-    @usstock = Usstock.new(usstock_params)
-    @usstock.save
-    response = RestClient.get "http://web.juhe.cn:8080/finance/stock/usa", :params => {:gid => @usstock.juhe_gid, :key => JUHE_CONFIG["api_key"]}
-    data = JSON.parse(response.body)
-
-      @usstock.update(:name => data["result"][0]["data"]["name"],
-                      :lastestpri => data["result"][0]["data"]["lastestpri"], :openpri => data["result"][0]["data"]["openpri"],
-                      :formpri => data["result"][0]["data"]["formpri"], :limit => data["result"][0]["data"]["limit"],
-                      :uppic => data["result"][0]["data"]["uppic"], :priearn => data["result"][0]["data"]["priearn"],
-                      :beta => data["result"][0]["data"]["beta"])
-
-    redirect_to usstock_path(@usstock)
-  end
-
-
   def show
     @usstock = Usstock.find(params[:id])
+    response = RestClient.get "http://web.juhe.cn:8080/finance/stock/usa", :params => {:gid => @usstock.juhe_gid, :key => JUHE_CONFIG["api_key"]}
+    data = JSON.parse(response.body)
+    @lastestpri = data["result"][0]["data"]["lastestpri"]
+    @openpri = data["result"][0]["data"]["openpri"]
+    @formpri = data["result"][0]["data"]["formpri"]
+    @limit = data["result"][0]["data"]["limit"]
+    @uppic = data["result"][0]["data"]["uppic"]
+    @priearn = data["result"][0]["data"]["priearn"]
+    @beta = data["result"][0]["data"]["beta"]
+    @ustime = data["result"][0]["data"]["ustime"]
   end
 
   private
--- test.rb
+++
@@ -1,7 +0,0 @@
-array = []
-
-for i in 1..10 do
-  array << i
-end
-
-puts array
